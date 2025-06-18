b252fd73e49e527d33221b8d9b083a7df44340db
whashim <whashim@gmail.com>
2025-06-19 07:56:34 +0800

    'update'

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+require('dotenv').config(); // Load environment variables from .env file
+
 app.get('/', (req, res) => {
-  res.send('index.js - Hello from Node.js on Cloud Run!');
+  res.send(`index.js - Hello from Node.js on Cloud Run! - ${process.env.ENV_VAR}`);
 });
 
 app.listen(port, () => {
